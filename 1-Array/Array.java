96fd54755f31061c8218ef659368dbc8c27e14da
sachinsingh <sachinseengh@gmail.com>
2024-02-17 20:40:12 +0545

    Array and linear Search

---
+++ 1-Array/Array.java
@@ -0,0 +1,8 @@
+public class Array {
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3 };
+
+        for (int i = 0; i < nums.length; i++)
+            System.out.println(nums[i]);
+    }
+}
---
+++ 1-Array/LinearSearch.java
@@ -0,0 +1,20 @@
+// package 1-Array;
+
+public class LinearSearch {
+    public static int linear_search(int[] nums, int target) {
+        int index = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+
+    public static void main(String[] args) {
+        int[] array = { 1, 3, 5, 8, 12, 34, 15 }; // array can be sorted or unsorted
+        int target = 345;
+        System.out.println(linear_search(array, target));
+    }
+}
