c3cff2e9207bb13f38ceece4ac936ffbd3c0623f
sachinsingh <sachinseengh@gmail.com>
2024-03-04 09:02:56 +0545

    binary tree traversal

--- 5-Tree/BinaryTree.java/BinaryTree.java
+++ 5-Tree/BinaryTree.java/BinaryTree.java
@@ -29,12 +29,51 @@ public class BinaryTree {
         }
     }
 
+    public static void preorder(Node root) {
+        if (root == null) {
+            System.out.print("-1,");
+            return;
+        }
+        System.out.print(root.data + ", ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    public static void inorder(Node root) {
+        if (root == null) {
+            System.out.print("-1,");
+            return;
+
+        }
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+
+    }
+
+    public static void postorder(Node root) {
+        if (root == null) {
+            System.out.print("-1,");
+            return;
+        }
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.data + " ");
+
+    }
+
     public static void main(String[] args) {
 
         int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
         BinaryTrees tree = new BinaryTrees();
         Node root = tree.buildTree(nodes);
         System.out.println(root.data);
+        // preorder(root);
+        // System.out.println("-----------------");
+        // inorder(root);
+
+        postorder(root);
 
     }
+
 }
